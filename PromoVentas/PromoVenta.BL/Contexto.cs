93574c7debea3a2a6383788560b5fa958dcab1a6
Nelson Contreras <Nelson Contreras@DESKTOP-A0JJ5SB>
2021-02-14 18:08:53 -0600

    Agregando DataBase y Nuevos Productos

---
+++ PromoVentas/PromoVenta.BL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoVenta.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base("PromosVentaDB")
+        {
+
+        }
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
